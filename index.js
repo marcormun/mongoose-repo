3610f4a7c0e488a33929405c5af3497491195a89
marcormun <mcordontrabajo@gmail.com>
2022-06-20 13:15:03 +0200

    feat:enviroment added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-    .then(()=> console.log('Connexion established'))
-    .catch((error)=> console.log('Error connetcing to MongoDB', error));
+
+//Port
+const PORT = process.env.PORT || 4000
 
 //routes
 
@@ -12,4 +13,11 @@ app.get('/', (req, res)=> {
     return res.send('Bienvenidos a mi aplicacion de tareas');
 });
 
-app.listen(3000, ()=> console.log('Server is running'));
+//connection mongodb
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> console.log('Connexion established'))
+    .catch((error)=> console.log('Error connetcing to MongoDB', error));
+
+
+app.listen(PORT, ()=> console.log('Server is running in' + PORT));
