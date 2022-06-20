baf6bf8833725b225a266a3cf2c86e0891035615
marcormun <mcordontrabajo@gmail.com>
2022-06-20 12:45:59 +0200

    feat: connection mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+    .then(()=> console.log('Connexion established'))
+    .catch((error)=> console.log('Error connetcing to MongoDB', error));
 
 //routes
 
