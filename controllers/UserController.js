15cc8dfd05d43e33f3c6888c2fe8da77a433ad15
marcormun <mcordontrabajo@gmail.com>
2022-06-21 13:09:51 +0200

    feat: getAll/post users refactor to user Controller

---
+++ controllers/UserController.js
@@ -0,0 +1,52 @@
+const User = require("../models/User");
+
+const userController = {};
+
+userController.getAll = async (req,res)=>{
+    try{
+        const users = await User.find();
+        
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Get all users retrieved succcesfully',
+                data: users
+            }
+        );
+    }catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error retrieving users',
+                error: error.message
+            }
+        )
+    }
+}
+
+userController.post = async(req,res)=>{
+    try{
+        const {username,email,password} = req.body;
+        const newUser = {
+            username,
+            email,
+            password
+        }
+        await User.create(newUser);
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Create user succesfully'
+            }
+        )
+    }catch(error){
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error creating user',
+                error: error?.message || error
+            }
+        )
+    }
+}
+module.exports = userController;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,53 +1,9 @@
-const User = require('../models/User');
+const userController = require('../controllers/UserController');
 const router = require('express').Router();
 
 
-router.get('/users', async (req,res)=>{
-    try{
-        const users = await User.find();
-        
-        return res.status(200).json(
-            {
-                success: true,
-                message: 'Get all users retrieved succcesfully',
-                data: users
-            }
-        );
-    }catch (error) {
-        return res.status(500).json(
-            {
-                success: false,
-                message: 'Error retrieving users',
-                error: error.message
-            }
-        )
-    }
-})
+router.get('/users', userController.getAll)
 
-router.post('/users', async(req,res)=>{
-    try{
-        const {username,email,password} = req.body;
-        const newUser = {
-            username,
-            email,
-            password
-        }
-        await User.create(newUser);
-        return res.status(200).json(
-            {
-                success: true,
-                message: 'Create user succesfully'
-            }
-        )
-    }catch(error){
-        return res.status(500).json(
-            {
-                success: false,
-                message: 'Error creating user',
-                error: error?.message || error
-            }
-        )
-    }
-})
+router.post('/users', userController.post)
 
 module.exports = router;
