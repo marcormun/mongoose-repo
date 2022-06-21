be1e3f4bc09ed9eba2a958a11469949c5ed84152
marcormun <mcordontrabajo@gmail.com>
2022-06-21 11:07:33 +0200

    feat:user model

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const db = require('./config/database');
+const User = require('./models/User')
 
 const app = express();
 
 //Port
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 //routes
+
+app.get('/users', async (req,res)=>{
+    try{
+        const users = await User.find();
+        
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Get all users retrieved succcesfully',
+                data: users
+            }
+        );
+    }catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error retrieving users'
+            }
+        )
+    }
+})
 
 app.get('/', (req, res)=> {
     return res.send('Bienvenidos a mi aplicacion de tareas');
 });
 
-//connection mongodb
+app.get('*',(req,res)=>{
+    return res.status(404).send('404 route not found')
+})
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=> console.log('Connexion established'))
-    .catch((error)=> console.log('Error connetcing to MongoDB', error));
+//connect database
 
-
-app.listen(PORT, ()=> console.log('Server is running in' + PORT));
+db().then(()=>{
+app.listen(PORT, ()=> console.log('Server is running in ' + PORT));
+}).catch((error)=>{
+    console.log('Error connecting to mongoDB ', error);
+})
