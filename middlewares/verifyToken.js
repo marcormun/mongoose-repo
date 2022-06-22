42462b3598d6bed8a5184e7c138b7962943d8f90
marcormun <mcordontrabajo@gmail.com>
2022-06-22 13:22:47 +0200

    feat: verify token middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next)=> {
+    try{
+        const {authorization} = req.headers;
+        if(!authorization){
+            return res.status(401).json({
+                success: false,
+                message: 'Token invalid'
+            })
+        }
+        
+        const token = authorization.split(' ')[1];
+
+        let decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!decoded){
+            return res.status(401).json({
+                success: false,
+                message: 'Token invalid 2'
+            })
+        }
+        next();
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: 'Token invalid 3'
+        })
+    }
+
+}
+module.exports = verifyToken;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const userController = require('../controllers/UserController');
+const verifyToken = require('../middlewares/verifyToken');
 const router = require('express').Router();
 
 
-router.get('/users', userController.getAll)
+router.get('/users',verifyToken, userController.getAll)
 router.get('/users/:id',userController.getUserById)
 router.put('/users/:id',userController.updateUser)
 router.delete('/users/:id',userController.deleteUser)
